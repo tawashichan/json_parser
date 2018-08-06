50388b08b3148a3c75264996ea05c3addc42b702
irieryota <ryotairie123@gmail.com>
2018-08-07 01:35:56 +0900

    parserを簡単に

--- src/parser.ml
+++ src/parser.ml
@@ -7,21 +7,8 @@ module Parser = struct
 
   let parse_list lst =
     let rec sub ls result = match ls with
-      | Lexer.STRING(s) :: Lexer.COLON :: Lexer.LBRACKET :: rest ->
-        let rec sub_ary re acm =
-          match re with
-            Lexer.RBRACKET :: r -> r,Ast.List acm
-          | _ ->
-            let (r,res) = sub re []
-            in sub_ary r (res :: acm)
-        in let (rr,l) = sub_ary rest [] (* ここでAst.Listを返す *)
-        in sub rr ((s,l) :: result)
-      | Lexer.STRING(s) :: Lexer.COLON ::Lexer.LBRACE :: rest ->
-        let (r,res) = sub rest []
+        Lexer.STRING(s) :: Lexer.COLON :: rest -> let (r,res) = sub rest []
         in sub r ((s,res) :: result)
-      | Lexer.STRING(s) :: Lexer.COLON :: Lexer.INT(i) :: rest -> sub rest ((s,Ast.Int i) :: result)
-      | Lexer.STRING(s) :: Lexer.COLON :: Lexer.FLOAT(f) :: rest -> sub rest ((s,Ast.Float f) :: result)
-      | Lexer.STRING(s) :: Lexer.COLON :: Lexer.STRING(v) :: rest -> sub rest ((s,Ast.String v) :: result)
       | Lexer.RBRACE :: rest -> rest,Ast.Assoc result
       | Lexer.COMMA :: rest -> sub rest result
       | Lexer.INT(i) :: rest -> rest,Ast.Int i
